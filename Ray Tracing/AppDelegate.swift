5d4260f34a584560b46ff676d31567ec1bf2c134
Mateus Rodrigues <mateus.fr26@gmail.com>
2019-03-25 10:17:24 -0300

    Initial Commit

---
+++ Ray Tracing/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Ray Tracing
+//
+//  Created by Mateus Rodrigues on 25/03/19.
+//  Copyright © 2019 Mateus Rodrigues. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Ray Tracing/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ray Tracing
+//
+//  Created by Mateus Rodrigues on 25/03/19.
+//  Copyright © 2019 Mateus Rodrigues. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
